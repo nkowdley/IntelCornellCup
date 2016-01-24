afd23add47d8a5cfbc083703a1ef450790f8cc7e
Swaroop Akkineni <swa7@pitt.edu>
2016-01-23 20:59:25 -0500

    Swaroop version_1 of streaming app
    First version of streaming app. Can only stream a video from youtube

---
+++ Drone_app_v1/app/src/main/java/com/example/swaroopakkineni/drone_app_v1/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.swaroopakkineni.drone_app_v1;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+/*
+ * This code is to accompany the Tutsplus tutorial:
+ * Streaming Video in Android Apps
+ *
+ * Sue Smith
+ * March 2014
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+        MediaController vidControl = new MediaController(this);
+        vidControl.setAnchorView(vidView);
+        vidView.setMediaController(vidControl);
+        String vidAddress =
+                "https://www.youtube.com/watch?v=hAxbO5a1JbM";
+        Uri vidUri = Uri.parse(vidAddress);
+        vidView.setVideoURI(vidUri);
+        vidView.start();
+    }
+
+}
+/*import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}*/
